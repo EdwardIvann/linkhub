af947c208e9a86ec5b7af9745cf17b789f78b8c3
Ivan Edward <iv4nedward@gmail.com>
2026-02-12 04:19:49 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    // Texto central e título
+    tituloPrincipal: "BANCO DE DADOS CESAR 2026.1",
+    subtitulo: "Links Úteis",
+    textoInformativo: "Página opensource para auxílio na organização das informações.",
+
+    // Identidade Visual
+    cores: {
+        fundo: "#e35a00",    // Laranja principal
+        botoes: "#f26b1d",   // Laranja dos links
+        texto: "#ffffff"     // Cor da fonte
+    },
+
+    // Lista de Links
+    meusLinks: [
+        { texto: "Acesso ao portal acadêmico", url: "http://cesar.lyceum.com.br/AOnline3/" },
+        { texto: "Calendário acadêmico 2026.1", url: "https://drive.google.com/file/d/1ULbQWXUe7CgrcBX6-bfT8BzfKI1kB8BD/view" },
+        { texto: "Horários - Turmas Banco de Dados", url: "https://docs.google.com/spreadsheets/d/1t08XxohH77ISgQHgYT_B6Ek5TbPCB3jhYTq13r8kREk/edit?gid=61666093#gid=61666093" },
+        { texto: "Manual do estudante 2025.2", url: "https://drive.google.com/file/d/1fpUxnOsYP123YRaCTnLq7jALZRvU9zox/view" },
+    ]
+};
